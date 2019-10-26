28e7da4013bbe2fe66ed4c9d7921e99edc4e99fe
Jianghai.Dai <jasonhavend@gmail.com>
2019-10-26 15:09:58 +0800

    Add files via upload

---
+++ mongo_export.sh
@@ -0,0 +1,3 @@
+mongoexport -d weibo -c user -o user.dat
+mongoexport -d weibo -c post -o post.dat
+mongoexport -d weibo -c comment -o comment.dat
---
+++ mongo_import.sh
@@ -0,0 +1,3 @@
+mongoimport -d weibo -c user -o user.dat
+mongoimport -d weibo -c post -o post.dat
+mongoimport -d weibo -c comment -o comment.dat
